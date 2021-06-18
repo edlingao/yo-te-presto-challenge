8d87681c2b4e9041f9f46d9079a8969ce593f4d0
edlingao <sonido244@gmail.com>
2021-06-17 22:37:44 -0500

    ( feat )Added scss support and styled components

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from '../svg/logo.svg';
+
+const MainBody = styled.div`
+  display: flex;
+  background: red;
+`;
 
 export default function Home() {
   return (
-    <div>
+    <MainBody>
       <h1>Hola mundo</h1>
       <img alt="Code Blog" src={logo} />
-    </div>
+    </MainBody>
   );
 }
